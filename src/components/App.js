687b0fed017c2b1af613dc979706aae384f86414
sanjeevkumar-j-v <sanjeevkumarjv@gmail.com>
2020-09-05 18:26:56 +0530

    Fetch Posts using API

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Alpenliebe</h1>
-      </header>
-    </div>
-  );
+import { fetchPosts } from '../actions/posts';
+
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(fetchPosts());
+  }
+  
+  render() {
+    console.log("Props : ",this.props)
+    return (
+      <div>
+        Welcome to Alpenliebe
+      </div>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    posts: state.posts
+  };
+}
+
+export default connect(mapStateToProps)(App);
