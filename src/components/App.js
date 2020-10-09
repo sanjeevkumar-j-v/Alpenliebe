55a85f83374455b94759157db6d593ba74082e9d
sanjeevkumar-j-v <sanjeevkumarjv@gmail.com>
2020-10-09 19:45:09 +0530

    Friends: Dispatch actions to display friends

--- src/components/App.js
+++ src/components/App.js
@@ -21,6 +21,7 @@ import {
 import jwt_decode from 'jwt-decode';
 import { authenticateUser } from '../actions/auth';
 import { getAuthTokenFromLocalStorage } from '../helpers/utils';
+import { fetchUserFriends } from '../actions/friends';
 
 const PrivateRoute = (privateRouteProps) => {
   const { isLoggedin, path, component: Component } = privateRouteProps;
@@ -56,6 +57,7 @@ class App extends React.Component {
 
     if (token) {
       const user = jwt_decode(token);
+
       console.log('user', user);
       this.props.dispatch(
         authenticateUser({
@@ -64,11 +66,13 @@ class App extends React.Component {
           name: user.name,
         })
       );
+
+      this.props.dispatch(fetchUserFriends());
     }
   }
 
   render() {
-    const { posts, auth } = this.props;
+    const { posts, auth, friends } = this.props;
     return (
       <Router>
         <div>
@@ -79,7 +83,14 @@ class App extends React.Component {
               exact
               path="/"
               render={(props) => {
-                return <Home {...props} posts={posts} />;
+                return (
+                  <Home
+                    {...props}
+                    posts={posts}
+                    friends={friends}
+                    isLoggedin={auth.isLoggedin}
+                  />
+                );
               }}
             />
             <Route path="/login" component={Login} />
@@ -106,6 +117,7 @@ function mapStateToProps(state) {
   return {
     posts: state.posts,
     auth: state.auth,
+    friends: state.friends,
   };
 }
 
